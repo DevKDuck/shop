7a64cb2471a22edb7a3db56afa64d35d17e5210d
DevKDuck <loading95@naver.com>
2024-09-05 17:23:26 +0900

    [validation 의존성 추가]회원 가입 성공시 메인페이지 리다이렉트, 정보 검증+중복가입에 의한 실패시 실패 이유 출력

--- duckshop/src/main/java/com/devkduck/duckshop/controller/MemberController.java
+++ duckshop/src/main/java/com/devkduck/duckshop/controller/MemberController.java
@@ -3,10 +3,12 @@ package com.devkduck.duckshop.controller;
 import com.devkduck.duckshop.dto.MemberFormDto;
 import com.devkduck.duckshop.entity.Member;
 import com.devkduck.duckshop.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,10 +25,31 @@ public class MemberController {
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
+
+    /* 회원가입시 유효성 검사 확인 전 코드
     @PostMapping(value = "/new")
     public String memberForm(MemberFormDto memberFormDto){
         Member member = Member.createMember(memberFormDto,passwordEncoder);
         memberService.saveMember(member);
+        return "redirect:/";
+    }
+    */
+
+
+    @PostMapping(value="/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "member/memberForm"; //에러 발생시 회원가입 이동
+        }
+
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        }
+        catch(IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm"; //예외 발생시 에러메시지 뷰로 전달
+        }
         return "redirect:/";
     }
 }
--- duckshop/src/main/java/com/devkduck/duckshop/dto/MemberFormDto.java
+++ duckshop/src/main/java/com/devkduck/duckshop/dto/MemberFormDto.java
@@ -1,14 +1,27 @@
 package com.devkduck.duckshop.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 //회원가입 화면으로부터 넘어오는 가입정보를 담을 dto
 @Getter
 @Setter
 public class MemberFormDto {
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
+
+    @NotEmpty(message = "이메일을 필수 입력값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요")
     private String email;
+
+    @NotEmpty(message = "비밀번호는 필수입니다.")
+    @Length(min=8, max=16, message= "비밀번호 8자 이상, 16자 이하로 입력해주세요")
     private String password;
+
+    @NotEmpty(message = " 주소는 필수 입력 값입니다.")
     private String address;
 }
