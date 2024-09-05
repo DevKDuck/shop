76e37d0c98238a4b2a5e213f30fe4e130ff37541
DevKDuck <loading95@naver.com>
2024-09-05 15:03:45 +0900

    회원정보를 저장하는 Member 엔티티 생성

---
+++ duckshop/src/main/java/com/devkduck/duckshop/entity/Member.java
@@ -0,0 +1,46 @@
+package com.devkduck.duckshop.entity;
+
+
+import com.devkduck.duckshop.constant.Role;
+import com.devkduck.duckshop.dto.MemberFormDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique=true)
+    private String email;
+
+    private String password;
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder) {
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        String password = passwordEncoder.encode(memberFormDto.getPassword()); //BCryPasswordEncoder Bean 을 파라미터로 넘겨 비밀번호 암호화
+        member.setPassword(password);
+        member.setRole(Role.USER);
+        return member;
+
+    }
+}
