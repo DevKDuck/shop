ac5108c7b0535cc8d854ef3b94d14f876d688a31
DevKDuck <loading95@naver.com>
2024-09-05 16:31:59 +0900

    회원가입 POST 요청시 회원가입 후, 메인으로 이동하도록 컨트롤러 설정

--- duckshop/src/main/java/com/devkduck/duckshop/controller/MemberController.java
+++ duckshop/src/main/java/com/devkduck/duckshop/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.devkduck.duckshop.controller;
 
 import com.devkduck.duckshop.dto.MemberFormDto;
+import com.devkduck.duckshop.entity.Member;
 import com.devkduck.duckshop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/members")
@@ -13,10 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value ="/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
+    }
+    @PostMapping(value = "/new")
+    public String memberForm(MemberFormDto memberFormDto){
+        Member member = Member.createMember(memberFormDto,passwordEncoder);
+        memberService.saveMember(member);
+        return "redirect:/";
     }
 }
