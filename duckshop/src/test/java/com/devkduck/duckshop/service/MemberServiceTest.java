6db218df9bfc908117489c68b8ccd5374b41b190
DevKDuck <loading95@naver.com>
2024-09-05 15:56:30 +0900

    🍀Unit Test🍀 회원 가입 기능 테스트

---
+++ duckshop/src/test/java/com/devkduck/duckshop/service/MemberServiceTest.java
@@ -0,0 +1,47 @@
+package com.devkduck.duckshop.service;
+
+import com.devkduck.duckshop.dto.MemberFormDto;
+import com.devkduck.duckshop.entity.Member;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+
+public class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@gmail.com");
+        memberFormDto.setName("박경덕");
+        memberFormDto.setAddress("서울시 마포구 합정동");
+        memberFormDto.setPassword("1234");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원 가입 기능 테스트")
+    public void saveMemberTest(){
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        //저장된 값과 실제 저장값 같은지 비교테스트
+        assertEquals(member.getEmail(), savedMember.getEmail());
+        assertEquals(member.getName(), savedMember.getName());
+        assertEquals(member.getAddress(), savedMember.getAddress());
+        assertEquals(member.getPassword(), savedMember.getPassword());
+        assertEquals(member.getRole(), savedMember.getRole());
+    }
+
+}
