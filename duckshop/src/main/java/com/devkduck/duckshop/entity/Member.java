6a393b6bd45f829619adc4058c016e25774c2ebb
DevKDuck <loading95@naver.com>
2024-09-07 21:42:24 +0900

    👾테스트코드👾admin 권한으로 상품 등록 페이지 접근 권한 테스트

--- duckshop/src/main/java/com/devkduck/duckshop/entity/Member.java
+++ duckshop/src/main/java/com/devkduck/duckshop/entity/Member.java
@@ -39,7 +39,8 @@ public class Member {
         member.setAddress(memberFormDto.getAddress());
         String password = passwordEncoder.encode(memberFormDto.getPassword()); //BCryPasswordEncoder Bean 을 파라미터로 넘겨 비밀번호 암호화
         member.setPassword(password);
-        member.setRole(Role.USER);
+//        member.setRole(Role.USER);
+        member.setRole(Role.ADMIN); //유저 -> 관리자 테스트
         return member;
 
     }
