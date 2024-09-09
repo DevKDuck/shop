ea813dc096fc893abba095af5822d7979369e898
DevKDuck <loading95@naver.com>
2024-09-09 16:17:26 +0900

    업로드한 파일을 읽어올 WebMvcConfig 생성

---
+++ duckshop/src/main/java/com/devkduck/duckshop/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.devkduck.duckshop.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${uploadPath}") // app.prop 에 설정한 upload 프로퍼티 값
+    String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**") //url에 images로 시작하는 경우 uploadPath에 설정한 폴더를 기준으로 파일 읽어오도록 설정
+                .addResourceLocations(uploadPath); //로컬에 저장된 파일 일어올 경로설정
+    }
+}
