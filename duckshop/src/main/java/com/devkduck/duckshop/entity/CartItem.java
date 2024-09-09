ab2740f6ac538b33fd6dc561a4c6b1ac32ad236e
DevKDuck <loading95@naver.com>
2024-09-09 10:13:44 +0900

    Cart Item 엔티티 설계

---
+++ duckshop/src/main/java/com/devkduck/duckshop/entity/CartItem.java
@@ -0,0 +1,32 @@
+package com.devkduck.duckshop.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "cart_item")
+public class CartItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    //하나의 장바구니에 여러개 상품
+    @ManyToOne
+    @JoinColumn(name="cart_id")
+    private Cart cart;
+
+    //하나의 상품은 여러 장바구니의 상품으로 담김
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private Item item;
+
+    //같은 상품 장바구니에 몇개 담는지 카운트
+    private int count;
+
+}
