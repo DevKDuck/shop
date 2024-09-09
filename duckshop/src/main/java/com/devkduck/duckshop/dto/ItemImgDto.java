adcaf23e90708b6053ff7a9c56641a230d97bd79
DevKDuck <loading95@naver.com>
2024-09-09 15:53:17 +0900

    상품 이미지 데이터 전달 DTO, 상품 데이터 정보 전달 DTO 생성, itemForm.html 기능 설정 및 화면 구성

---
+++ duckshop/src/main/java/com/devkduck/duckshop/dto/ItemImgDto.java
@@ -0,0 +1,29 @@
+package com.devkduck.duckshop.dto;
+
+import com.devkduck.duckshop.entity.ItemImg;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+/*
+상품 저장후 상품 이미지에 대한 데이터를 전달할 클래스
+ */
+@Getter
+@Setter
+public class ItemImgDto {
+    private Long id;
+
+    private String imgName;
+
+    private String oriImgName;
+
+    private String imgUrl;
+
+    private String repImgYn;
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public static ItemImgDto toDto(ItemImg itemImg) {
+        return modelMapper.map(itemImg, ItemImgDto.class);
+    }
+}
