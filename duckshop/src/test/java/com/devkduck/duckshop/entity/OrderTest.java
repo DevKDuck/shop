cee485308eb553ff10fc3e150234eaf6bc80d0b8
DevKDuck <loading95@naver.com>
2024-09-09 11:49:08 +0900

    고아 객체 제거 테스트

--- duckshop/src/test/java/com/devkduck/duckshop/entity/OrderTest.java
+++ duckshop/src/test/java/com/devkduck/duckshop/entity/OrderTest.java
@@ -2,6 +2,7 @@ package com.devkduck.duckshop.entity;
 
 import com.devkduck.duckshop.constant.ItemSellStatus;
 import com.devkduck.duckshop.repository.ItemRepository;
+import com.devkduck.duckshop.repository.MemberRepository;
 import com.devkduck.duckshop.repository.OrderRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
@@ -27,6 +28,9 @@ public class OrderTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @PersistenceContext
     EntityManager em;
@@ -68,4 +72,31 @@ public class OrderTest {
         assertEquals(3,savedOrder.getOrderItems().size());
     }
 
+
+    public Order createOrder(){
+        Order order = new Order();
+        for(int i=0;i<3;i++){
+            Item item = createItem();
+            itemRepository.save(item);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(1000);
+            orderItem.setOrder(order);
+            order.getOrderItems().add(orderItem);
+        }
+        Member member = new Member();
+        memberRepository.save(member);
+        order.setMember(member);
+        orderRepository.save(order);
+        return order;
+    }
+
+    @Test
+    @DisplayName("고아 객체 제거 테스트")
+    public void orphanRemovalTest(){
+        Order order = this.createOrder();
+        order.getOrderItems().remove(0);
+        em.flush();
+    }
 }
