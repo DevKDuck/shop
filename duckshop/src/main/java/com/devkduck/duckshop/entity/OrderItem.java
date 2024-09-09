210f5c3baf166e0f0543db48fe36141f452c55ba
DevKDuck <loading95@naver.com>
2024-09-09 11:04:18 +0900

    주문 상품 엔티티 설정

---
+++ duckshop/src/main/java/com/devkduck/duckshop/entity/OrderItem.java
@@ -0,0 +1,38 @@
+package com.devkduck.duckshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    //하나의 상품은 여러 주문 상품에 들어갈 수 있음
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    //한번의 주문은 여러개의 상품을 주문할 수 있음
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    //주문 가격
+    private int orderPrice;
+
+    private int count;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+
+}
