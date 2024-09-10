617c774a675152d0fb4408016899e1f8374d1b55
DevKDuck <loading95@naver.com>
2024-09-10 09:28:30 +0900

    price와 stockNumber는 Integer 타입으로 NotBlank 대신 NotNull 로 변경

--- duckshop/src/main/java/com/devkduck/duckshop/dto/ItemFormDto.java
+++ duckshop/src/main/java/com/devkduck/duckshop/dto/ItemFormDto.java
@@ -3,6 +3,7 @@ package com.devkduck.duckshop.dto;
 import com.devkduck.duckshop.constant.ItemSellStatus;
 import com.devkduck.duckshop.entity.Item;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.modelmapper.ModelMapper;
@@ -18,13 +19,13 @@ public class ItemFormDto {
     @NotBlank(message = "상품명은 필수 입력 값입니다.")
     private String itemNm;
 
-    @NotBlank(message = "가격은 필수 입력 값입니다.")
+    @NotNull(message = "가격은 필수 입력 값입니다.")
     private Integer price;
 
     @NotBlank(message = "설명은 필수 입력 값입니다.")
     private String itemDetail;
 
-    @NotBlank(message = "재고는 필수 입력 값입니다.")
+    @NotNull(message = "재고는 필수 입력 값입니다.")
     private Integer stockNumber;
 
     private ItemSellStatus itemSellStatus;
