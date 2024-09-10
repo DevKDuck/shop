4df9217b52b47d58a7e70d1688baf954979cc618
DevKDuck <loading95@naver.com>
2024-09-10 10:58:42 +0900

    상품 수정 기능 추가

--- duckshop/src/main/java/com/devkduck/duckshop/service/ItemImgService.java
+++ duckshop/src/main/java/com/devkduck/duckshop/service/ItemImgService.java
@@ -2,21 +2,20 @@ package com.devkduck.duckshop.service;
 
 import com.devkduck.duckshop.entity.ItemImg;
 import com.devkduck.duckshop.repository.ItemImgRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
-
-import java.io.IOException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ItemImgService {
 
-    @Value("${itemImgLocation}") //app.prop 등록한 itemImgLocation 불러옴
+    @Value("${itemImgLocation}")
     private String itemImgLocation;
 
     private final ItemImgRepository itemImgRepository;
@@ -24,18 +23,37 @@ public class ItemImgService {
     private final FileService fileService;
 
     public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception{
-        String originalFileName = itemImgFile.getOriginalFilename();
+        String oriImgName = itemImgFile.getOriginalFilename();
         String imgName = "";
         String imgUrl = "";
 
         //파일 업로드
-        if(!StringUtils.isEmpty(originalFileName)){
-            imgName = fileService.uploadFile(itemImgLocation, originalFileName,itemImgFile.getBytes());
+        if(!StringUtils.isEmpty(oriImgName)){
+            imgName = fileService.uploadFile(itemImgLocation, oriImgName,
+                    itemImgFile.getBytes());
             imgUrl = "/images/item/" + imgName;
         }
 
         //상품 이미지 정보 저장
-        itemImg.updateItemImg(originalFileName, imgName, imgUrl);
+        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
+    }
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception{
+        if(!itemImgFile.isEmpty()){
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId)
+                    .orElseThrow(EntityNotFoundException::new);
+
+            //기존 이미지 파일 삭제
+            if(!StringUtils.isEmpty(savedItemImg.getImgName())) {
+                fileService.deleteFile(itemImgLocation+"/"+
+                        savedItemImg.getImgName());
+            }
+
+            String oriImgName = itemImgFile.getOriginalFilename();
+            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            String imgUrl = "/images/item/" + imgName;
+            savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        }
     }
 }
