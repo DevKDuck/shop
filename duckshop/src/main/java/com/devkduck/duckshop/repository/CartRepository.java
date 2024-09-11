74d40196381ca12a3a0de7361805f8efc94a9b47
DevKDuck <loading95@naver.com>
2024-09-11 12:13:05 +0900

    상품을 장바구니에 담는 기능 구현

--- duckshop/src/main/java/com/devkduck/duckshop/repository/CartRepository.java
+++ duckshop/src/main/java/com/devkduck/duckshop/repository/CartRepository.java
@@ -4,4 +4,7 @@ import com.devkduck.duckshop.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    //현재 로그인한 회원의 장바구니 찾기
+    Cart findByMemberId(Long memberId);
 }
