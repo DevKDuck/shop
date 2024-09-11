7497069bd63ce9093979b97ff295297732619b1c
DevKDuck <loading95@naver.com>
2024-09-11 12:18:20 +0900

    장바구니에 상품 담는 로직 테스트

---
+++ duckshop/src/test/java/com/devkduck/duckshop/service/CartServiceTest.java
@@ -0,0 +1,75 @@
+package com.devkduck.duckshop.service;
+
+
+import com.devkduck.duckshop.constant.ItemSellStatus;
+import com.devkduck.duckshop.dto.CartItemDto;
+import com.devkduck.duckshop.entity.CartItem;
+import com.devkduck.duckshop.entity.Item;
+import com.devkduck.duckshop.entity.Member;
+import com.devkduck.duckshop.repository.CartItemRepository;
+import com.devkduck.duckshop.repository.ItemRepository;
+import com.devkduck.duckshop.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations="classpath:application-test.properties")
+class CartServiceTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    CartService cartService;
+
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    //상품 생성
+    public Item saveItem(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        return itemRepository.save(item);
+    }
+
+    //회원정보 생성
+    public Member saveMember(){
+        Member member = new Member();
+        member.setEmail("test@test.com");
+        return memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("장바구니 담기 테스트")
+    public void addCart(){
+        Item item = saveItem();
+        Member member = saveMember();
+
+        CartItemDto cartItemDto = new CartItemDto();
+        cartItemDto.setCount(5);
+        cartItemDto.setItemId(item.getId());
+
+        Long cartItemId = cartService.addCart(cartItemDto, member.getEmail());
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        assertEquals(item.getId(), cartItem.getItem().getId());
+        assertEquals(cartItemDto.getCount(), cartItem.getCount());
+    }
+
+}
