883857b660a8e7f1907abae905f98eb9a012904f
DevKDuck <loading95@naver.com>
2024-10-14 11:10:48 +0900

    deploy test push

--- duckshop/src/main/java/com/devkduck/duckshop/controller/MainController.java
+++ duckshop/src/main/java/com/devkduck/duckshop/controller/MainController.java
@@ -27,7 +27,6 @@ public class MainController {
         model.addAttribute("items", items);
         model.addAttribute("itemSearchDto", itemSearchDto);
         model.addAttribute("maxPage", 5);
-
         return "main";
     }
 
