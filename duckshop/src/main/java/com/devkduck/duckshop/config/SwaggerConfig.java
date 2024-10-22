573e7db7489b153a9bc2ac2daff54ebd2e8ffdb6
DevKDuck <loading95@naver.com>
2024-10-22 12:06:08 +0900

    [test]Swagger config, api

---
+++ duckshop/src/main/java/com/devkduck/duckshop/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.devkduck.duckshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("duckshop swagger")
+                .description("duckshop REST API")
+                .version("1.0.0");
+    }
+}
